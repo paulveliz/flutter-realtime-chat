8a575f4d06cce62852ec1c908e0eb068c12a7f5d
Paul guadalupe veliz lopez <paulsystem32@gmail.com>
2020-12-02 20:54:07 -0700

    Diseño del login
    Comenzamos haciendo la carcasa para empezar a hacerlo bonito.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,75 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Usuarios page')),
+      backgroundColor: Color(0xffF2F2F2),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _Logo(),
+            _Form(),
+            _Labels(),
+            Text('Terminos y condiciones de uso', style: TextStyle(fontWeight: FontWeight.w200))
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.only(top: 50),
+        width: 170,
+        child: Column(
+          children: [
+            Image(image: AssetImage('assets/tag-logo.png')),
+            SizedBox(height: 20),
+            Text('Messenger', style: TextStyle(fontSize: 30))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          TextField(),
+          TextField(),
+          RaisedButton(onPressed: (){})
+        ],
+      ),
+    );
+  }
+}
+
+class _Labels extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text('No tienes cuenta?', style: TextStyle(color: Colors.black54, fontSize: 15, fontWeight: FontWeight.w300)),
+          SizedBox(height: 10),
+          Text('Crea una ahora!', style: TextStyle(color: Colors.blue[600], fontSize: 18, fontWeight: FontWeight.bold))
+        ],
+      ),
     );
   }
 }
